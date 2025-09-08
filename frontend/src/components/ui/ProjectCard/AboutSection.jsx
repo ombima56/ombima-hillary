61b59c8cc4588b07cbf25159475699df0b600d7d
ombima56 <ombimahillary6@gmail.com>
2025-09-08 16:50:27 +0300

    chore: change the about picture

--- frontend/src/components/ui/ProjectCard/AboutSection.jsx
+++ frontend/src/components/ui/ProjectCard/AboutSection.jsx
@@ -165,11 +165,11 @@ const AboutSection = () => {
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <img
-          src="/images/aboutme.jpg"
+          src="/images/aboutme2.png"
           alt="about-image"
           width={500}
           height={500}
-          className="rounded-lg shadow-lg"
+          className="rounded-xl shadow-lg bg-primary-900 bg-opacity-50 object-contain aspect-square"
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
